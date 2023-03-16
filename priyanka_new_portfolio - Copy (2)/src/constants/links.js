c27adf35702663b6dde0e398c753092f9a5c6010
priyanka1565 <priyankaingle250@gmail.com>
2023-03-16 14:15:26 +0530

    added

--- priyanka_new_portfolio - Copy (2)/src/constants/links.js
+++ priyanka_new_portfolio - Copy (2)/src/constants/links.js
@@ -1,5 +1,5 @@
 export const ASSETS = {
   LOGO: "https://res.cloudinary.com/dfjdtufsq/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1667578168/samples/animals/my-logo_ckihba.jpg",
   PROFILE:
-    "https://res.cloudinary.com/dfjdtufsq/image/upload/v1678893604/myphoto_vsxepw.png",
+    "https://res.cloudinary.com/dfjdtufsq/image/upload/v1667578467/samples/ppp3_p00pyn.png",
 };
