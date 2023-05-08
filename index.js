96ec34ccb6a03960ea561233815aa013b20aad28
priyanka1565 <priyankaingle250@gmail.com>
2023-05-08 20:24:07 +0530

    update resume

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ toggle.addEventListener("click", ()=> {
     // GitHubCalendar(".calendar", "Yunuslala", { responsive: true });
 
     function downloadresume(){
-        window.open("https://drive.google.com/file/d/1tzBJQyJnqIRpQY401dhZBt5XPKL6Ow91/view?usp=share_link","_blank")
+        window.open(
+          "https://drive.google.com/file/d/13udb-5mGQFagtdgAykN53TY7a-OqrvOB/view?usp=share_link",
+          "_blank"
+        );
 
     }
     // document.querySelector(".mobile-menu").addEventListener("click",()=>{
